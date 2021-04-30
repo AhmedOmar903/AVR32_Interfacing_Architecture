b8c6b4a4f0d9249f065cecdb4eecb50d9b8452c4
AhmedOmar903 <69205902+AhmedOmar903@users.noreply.github.com>
2021-04-30 05:43:11 +0200

    Delete 01-APP/Osiliscope directory

--- 01-APP/Osiliscope/OSILLOSCOPE_interface.h
+++
@@ -1,9 +0,0 @@
-#ifndef _OSILLOSCOPE_INTERFACE_H_
-#define _OSILLOSCOPE_INTERFACE_H_
-
-/*Config TImer1 on InputCapture Mode With  Prescaller 8*/
-
-void OSCILLISCOPE_VoidInitialization(void);
-void OSCILLISCOPE_VoidGetTheReadingsOfTheSignalSyncrounase(u32 *PointerToFrequancy,u32 *PointerToDutyCycle);
-
-#endif
